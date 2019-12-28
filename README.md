30c75ae1f7bd4aa7da5f09a4a6263b96c829df3f
MFG38 <55315235+MFG38@users.noreply.github.com>
2019-12-28 16:20:48 +0200

    New script: battleship

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ A collection of Python programming experiments (and maybe proper tools later).
 
 ## Scripts and their descriptions
 
+* **battleship.py:** A simple game of Battleship. 'Nuff said.
 * **code_eval.py:** A simple prompt that asks how many lines of code you have and prints a comment based on your input. Mainly done for practice.
 * **looptest.py:** Testing a looping script. You can talk to it. Sort of.
 * **luckynumgen.py:** Feed this script a number and it generates a random one in the range of 1 and the number you entered.
---
+++ battleship/battleship.py
@@ -0,0 +1,53 @@
+# A simple game of Battleship. 'Nuff said.
+# Written in Notepad++, tested with Python 2.7.16 running
+# through the Windows command line.
+
+# Santtu "MFG38" Pesonen, 2019-12-28
+
+from random import randint
+
+board = []
+for i in range(8):
+    board.append(['O'] * 8)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+def random_row(board):
+    return randint(0,len(board) - 1)
+
+def random_col(board):
+    return randint(0,len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+#print ship_row
+#print ship_col
+
+for turn in range(10):
+    print "Turn", turn + 1
+    print_board(board)
+    
+    guess_row = int(raw_input("Guess row: "))
+    guess_col = int(raw_input("Guess column: "))
+    
+    if guess_row == ship_row and guess_col == ship_col:
+        print "AAAAAAAAA I'M DROWNING gg you found my ship."
+        break
+    else:
+        if guess_row not in range(0,8) or guess_col not in range(0,8):
+            print "ERROR: Guess out of bounds!"
+        elif board[guess_row][guess_col] == "X":
+            print "ERROR: Nothing already found there!"
+        else:
+            print "Hah, nope! Not over there!"
+            board[guess_row][guess_col] = "X"
+        
+        if turn == 9:
+            print "GAME OVER"
+    
+    
+    #print "Turn", turn + 1
+    #print_board(board)
+    
