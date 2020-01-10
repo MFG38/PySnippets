e8f19dc10ca73c8bbf31688a03c90c61fde5585c
MFG38 <55315235+MFG38@users.noreply.github.com>
2020-01-10 14:45:44 +0200

    Progress update to FoldZipper (not functional)

--- foldzipper/foldzipper.py
+++ foldzipper/foldzipper.py
@@ -2,3 +2,17 @@
 
 import os
 import zipfile
+
+# Create ZIP with all files and subfolders within the source folder.
+def pack_folder(src,dest):
+    relroot = os.path.abspath(os.path.join(src,os.pardir))
+    
+    with zipfile.ZipFile(dest,'w',zipfile.ZIP_DEFLATED) as zip:
+        for root, dirs, files in os.walk(src):
+            zip.write(root,os.path.relpath(relroot))
+            for file in files:
+                filename = os.path.join(root,file)
+                if os.path.isfile(filename):
+                    arcname = os.path.join(os.path.relpath(root,relroot),file)
+                    zip.write(arcname,filename)
+        zip.close()
