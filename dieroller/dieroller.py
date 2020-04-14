3f31d7633b8100f76e8894a3ae2cabe6371cd9c6
MFG38 <55315235+MFG38@users.noreply.github.com>
2020-04-14 19:57:07 +0300

    Update to dieroller.py (non-functional)

--- dieroller/dieroller.py
+++ dieroller/dieroller.py
@@ -1,4 +1,17 @@
 # To become a DnD die roller (take 2).
 
-while True:
-    roll = raw_input("Enter roll: ")
+from random import randint
+
+# while True:
+roll = 0
+getValues = raw_input("Enter roll: ")
+
+def doRoll(expression):
+    numdice = getValues.split("d")[0]
+    sides = int(getValues.split("d")[1])
+    modifier = 0
+        
+    roll = ((numdice * int(randint(1,sides+1))) + modifier)
+    return roll
+    print roll
+
