48f4ad1ff6749fba36c7aed3b0e7d36abd0128dc
MFG38 <55315235+MFG38@users.noreply.github.com>
2020-06-11 17:29:12 +0300

    More phrases for InsultGen

--- insultgen/insultgen.py
+++ insultgen/insultgen.py
@@ -1,6 +1,6 @@
 # Generates a random insult from a pre-determined
 # selection of words and phrases.
-# Current number of possible combinations: 960.
+# Current number of possible combinations: 1,232.
 # Written in Notepad++, tested with Python 2.7.16
 # running through the Windows command line.
 
@@ -20,7 +20,9 @@ adj = [
     "hollow-skulled ",
     "braindead ",
     "bone-headed ",
-    "gelatinous "]
+    "gelatinous ",
+    "mindless ",
+    "thoughtless "]
 
 noun = [
     "pile ",
@@ -42,7 +44,8 @@ thing = [
     "Satan's semen",
     "pubic hair",
     "yesterday's lunch",
-    "trash"]
+    "trash",
+    "fossilized dung"]
 
 #def geninsult():
 #adjindex = random.randint(0,len(adj)-1)
