38cc2c2f70774ce5f668e9f7cb88e538c3c0bfb0
MFG38 <55315235+MFG38@users.noreply.github.com>
2020-07-24 14:40:39 +0300

    Update to PasswordGen (non-functional)

--- passwordgen/passwordgen.py
+++ passwordgen/passwordgen.py
@@ -1,8 +1,11 @@
 # (UNFINISHED) Generates a random password that's 8-32 characters in length.
 
-chars =
+import random
+#import itertools
+
+chars = [
     # Upper-case letters
-    ["A", "B", "C", "D", "E", "F", "G", "H", "I",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I",
     "J", "K", "L", "M", "N", "O", "P", "Q", "R",
     "S", "T", "U", "V", "W", "X", "Y", "Z",
     # Lower-case letters
@@ -12,14 +15,21 @@ chars =
     # Numbers
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
+generatedList = []
+generatedPW = ""
+
 minLength = 8
 maxLength = 32
-passLength = int(raw_input("Enter desired length of password (8-32 characters): "))
 
 def genPassword(length):
-    if length < minLength or length > maxLength:
+    if length not in range(minLength,maxLength):
         print "Can't generate password: length is out of bounds!"
     else:
-        for n in length:
-            c[n] = random.choice(chars)
-        print "Your password is: "
+        for i in xrange(length):
+            generatedList.append(random.choice(chars))
+        generatedPW = "".join(generatedList)
+        return generatedPW
+
+passLength = int(raw_input("Enter desired length of password (8-32 characters): "))
+genPassword(passLength)
+print "Your password is: " + generatedPW
